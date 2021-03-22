ae15350b3acb8b85f56afa8101cbc784c9cc601b
RoelReijn <info@roelreijneveld.nl>
2021-03-22 19:26:06 +0100

    :construction: Security page v0.2

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -1,6 +1,53 @@
+<?php
+
+return [
+    'errors' => [
+        'pagecontent' => 'An error has occured when trying to load the page content.',
+    ],
+    'account' => [
+        'title' => 'Account',
+        'security' => [
+            'title' => 'Security Settings',
+            'description' => 'On this page you are able to adjust your security settings. You can set up a two-factor authentication device and you can see the devices you are logged into. You\'re also able to change your password.',
+            '2fa' => [
+                'title' => 'Two Factor Authentication',
+                'description' => 'Activating two-factor authentication protects your account in the best possible way. When you are logged in with your email address and password, we ask you to fill in a 2FA code to verify that it is really you. More information can be found here.',
+                'enabled' => '2FA is currently <b>enabled</b>',
+                'disabled' => '2FA is currently <b>disabled</b>',
+                'enable' => [
+                    'openbutton' => 'Enable 2FA (TOTP)',
+                    'title' => 'Activate Two Factor Authentication',
+                    'description' => 'Scan the QR code below with a TOTP app on your phone. Examples of such apps are Google Authenticator, Authy and Aegis. Once you have scanned the QR code you can press Finish, 2FA is then successfully activated.',
+                    'finish' => 'Finish',
+                    'cancel' => 'Cancel',
+                ],
+                'recovery' => [
+                    'openbutton' => 'Recovery Codes',
+                    'title' => 'Recovery Codes',
+                    'description' => 'Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.',
+                    'regenerate' => 'Regenerate Recovery Codes',
+                    'close' => 'Close',
+                ],
+                'disable' => [
+                    'openbutton' => 'Disable 2FA',
+                    'title' => 'Are you sure?',
+                    'description' => 'Do you really want to disable Two Factor Authentication? Your account may be more vulnerable to attacks/hacks if you proceed.',
+                    'proceed' => 'Proceed',
+                    'cancel' => 'Cancel',
+                ],
+            ],
+            'password' => [
+                'title' => 'Change Password',
+                'description' => 'You can change your password below. Do this by entering your current password and then your new password plus a confirmation of that. Then press save. Have you forgotten your current password? Please contact customer service or use the forgot password function when you\'re logged out.',
+                'current' => 'Current Password',
+                'new' => 'New Password',
+                'confirm' => 'Confirm New Password',
+                'save' => 'Save',
+            ],
             'session' => [
                 'title' => 'Active Sessions',
             ],
+        ],
         'address' => [
             'title' => 'Address Details',
         ],
@@ -16,3 +63,8 @@
                 'lightdescription' => 'Always display the portal in light mode.',
             ],
         ],
+        'active' => 'Active',
+        'pending' => 'Pending',
+        'inactive' => 'Inactive',
+    ],
+];
--- resources/views/admin/account/sidebar.blade.php
+++ resources/views/admin/account/sidebar.blade.php
@@ -1,3 +1,32 @@
+<div class="d-none d-lg-block col-lg-3">
+    <ul class="nav nav-pills nav-vertical">
+        <li class="nav-item">
+            <a href="{{ route('account.security') }}" class="nav-link {{ (request()->is('account/security*')) ? 'active' : '' }}">
+                @lang('admin.account.security.title')
+            </a>
+            <ul class="nav nav-pills" id="security-submenu">
+                <li class="nav-item">
+                    <a href="{{ route('account.security') }}#2fa" class="nav-link">
+                        @lang('admin.account.security.2fa.title')
+                        @if(!auth()->user()->two_factor_secret)
+                            <span class="badge bg-danger ms-auto" id="statusLabel2FA">@lang('admin.account.inactive')</span>
+                        @else
+                            <span class="badge bg-success ms-auto" id="statusLabel2FA">@lang('admin.account.active')</span>
+                        @endif
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('account.security') }}#password" class="nav-link">
+                        @lang('admin.account.security.password.title')
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('account.security') }}#sessions" class="nav-link">
+                        @lang('admin.account.security.session.title')
+                    </a>
+                </li>
+            </ul>
+        </li>
         <li class="nav-item">
             <a href="{{ route('account.address') }}" class="nav-link {{ (request()->is('account/address*')) ? 'active' : '' }}">
                 @lang('admin.account.address.title')
@@ -8,3 +37,5 @@
                 @lang('admin.account.preferences.title')
             </a>
         </li>
+    </ul>
+</div>
